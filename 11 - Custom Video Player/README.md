5e939fe14fffc18f451d5dc614bd8301a2a51a90
yan <1550529084@qq.com>
2022-04-29 13:54:50 +0800

    Finish 11

---
+++ 11 - Custom Video Player/README.md
@@ -0,0 +1,51 @@
+<br/>
+
+>简介：了解到[JavaScript30](https://javascript30.com)的30天挑战，本人开始进行挑战，并在项目中进行学习、思考。项目的完成是根据实现效果分析步骤，然后一步一步地利用所学知识、搜索引擎、参考[未枝丫](https://github.com/soyaine)的中文指南所完成的。
+
+<br/>
+
+# 11 Custom Video Player
+
+## 1. 实现效果
+**定制播放器**：页面展示视频、进度条，当用户在视频上悬浮鼠标时，页面出现视频控制按钮，可通过点击按钮或滑动滚动条改变视频播放属性，如暂停、快进、放大音量、倍速播放等。
+
+## 2. 使用到的关键js语法
+关于video的语法：
+```javascript
+video.currentTime;
+video.duration;
+video.paused;
+video.playbackRate；
+video.volume;
+video.play();
+video.addEventListener("timeupdate", f);
+```
+其他语法：
+```javascript
+event.offsetX;
+element.offsetWigth;
+```
+
+## 3. 使用到的关键css语法
+```css
+position: relative;
+position: absolute;
+background: none;
+cursor: pointer;
+cursor: vertical-text;
+input[type=range]::-webkit-slider-thumb
+input[type=range]::-webkit-slider-runnable-track
+input[type=range]::-moz-range-thumb
+input[type=range]::-moz-range-track 
+```
+
+## 4. 使用到的关键html语法
+```html
+<input type="range" min="" max="">
+```
+
+## 4. 难点/细节
+1.在div中水平移动或点击，都会相应改变视频播放进度和div大小
+2.设置两位整数的时间展示
+3.视频变化的事件"timeupdate"
+4.设置底部的控制区在悬浮时才上滑
---
+++ 11 - Custom Video Player/index.js
@@ -0,0 +1,81 @@
+const video = document.querySelector(".video");
+const progress_slider = document.querySelector(".bottom_progress_slider");
+const progress = document.querySelector(".bottom_progress_slider_juice");
+const pause_btn = document.querySelector(".bottom_controls_pause_btn");
+const show_time = document.querySelector(".bottom_controls_time");
+const volume_slider = document.querySelector(".bottom_controls_volume_slider");
+const speed_slider = document.querySelector(".bottom_controls_speed_slider");
+const forward_btn = document.querySelector(".bottom_controls_fastforward_btn");
+const rewind_btn = document.querySelector(".bottom_controls_rewind_btn");
+// 播放或暂停视频，视频播放并且改变pause——btn的text
+function pause_play() {
+    if (video.paused) {
+        this.innerText = "►";
+        video.play();
+    }
+    else {
+        this.innerText = '❚❚';
+        video.pause();
+    }
+};
+// 随着视频的播放，获取当前时间和总时间，更新当前时间
+// 设置时间的格式，保持两位整数
+function time_format(seconds) {
+    let minute = ("0" + parseInt(seconds / 60)).slice(-2);
+    let second = ("0" + parseInt(seconds % 60)).slice(-2);
+    let timeString = `${minute}:${second}`;
+    return timeString;
+};
+function time_show() {
+    const nowTime = time_format(video.currentTime);
+    const endTime = time_format(video.duration);
+    show_time.innerText = `${nowTime} / ${endTime}`;
+};
+// 随着视频的播放，通过当前时间和总时间调整进度条
+function progress_update() {
+    const played_percentage = video.currentTime / video.duration;
+    progress.style.width = played_percentage * 100 + "%";
+};
+// 可以移动进度条，鼠标移动的抬起点，鼠标的点击点，都是新的进度
+//
+function progress_change(e) {
+    // 获取元素的宽度
+    // console.log(getComputedStyle(this, null)["width"]);
+    // offset系列
+    const time_point = (e.offsetX / progress_slider.offsetWidth) * video.duration;
+    video.currentTime = time_point;
+};
+// 拉动slider，获取value以改变音量、速度
+function volume_change() {
+    video.volume = volume_slider.value;
+};
+function speed_change() {
+    video.playbackRate = speed_slider.value;
+};
+// 点击btn，video时间改变
+function forward() {
+    video.currentTime += 10;
+};
+function rewind() {
+    video.currentTime -= 10;
+};
+
+function main() {
+    pause_btn.addEventListener('click', pause_play);
+    video.addEventListener("timeupdate", time_show);
+    video.addEventListener("timeupdate", progress_update);
+    // progress_slider.addEventListener("click", progress_change);
+    // 拖动过程也会更新播放进度，需要限制触发条件，当mousedown时，mousemove才触发，并在mouseup结束。
+    let mousedown = false;
+    progress_slider.addEventListener("mousedown", () => mousedown = true);
+    progress_slider.addEventListener("mouseup", () => mousedown = false);
+    progress_slider.addEventListener("mousemove", function (e) { if (mousedown) progress_change(e) });
+    volume_slider.addEventListener("change", volume_change);
+    speed_slider.addEventListener("change", speed_change);
+    forward_btn.addEventListener("click", forward);
+    rewind_btn.addEventListener("click", rewind);
+}
+
+export {
+    main
+}
